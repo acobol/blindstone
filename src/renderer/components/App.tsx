22992f124c4d75bc48a775871c25b7bb08d6f81a
Andrés Cobo Laguna <andres.cobo.laguna@gmail.com>
2020-06-29 20:17:40 +0200

    feat(ui): Add initial hardcoded menu

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -2,10 +2,16 @@ import { hot } from "react-hot-loader";
 import React from "react";
 
 const App = (): JSX.Element => {
-  return (<div>
-    <h1>💖 Hello World!</h1>
-  </div>);
+  return (
+    <div>
+      <h1>MAIN MENU</h1>
+      <nav>
+        <button>Jugar</button>
+        <button>Aventuras</button>
+        <button>Pelea de taberna</button>
+        <button>Modos</button>
+      </nav>
+    </div>
+  );
 };
 export default hot(module)(App);
-
-//export default App;
